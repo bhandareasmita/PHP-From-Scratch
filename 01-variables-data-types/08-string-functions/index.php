f23ba994068f3ff8ec1365e0bd1dc74efd0aafb8
Asmita Bhandare <20106482@tafe.wa.edu.au>
2024-03-15 19:40:53 +0800

    Feat: Complete exercise-1.8

--- 01-variables-data-types/08-string-functions/index.php
+++ 01-variables-data-types/08-string-functions/index.php
@@ -1,5 +1,37 @@
 <?php
 $output = null;
+$string = 'Hello World';
+
+// strlen
+$output = strlen($string);
+
+// str_word_count
+$output = str_word_count($string);
+
+// strpos
+$output = strpos($string, 'World');
+
+// Get specific char by index
+$output = $string[4];
+
+// substr
+$output = substr($string, 6, 5);
+
+// str_replace
+$output = str_replace('World', 'Universe', $string);
+
+// strtolower
+$output = strtolower($string);
+
+// strtoupper
+$output = strtoupper($string);
+
+// ucwords
+$output = ucwords($string);
+
+$output = trim('    Hello World    ');
+
+
 ?>
 
 <!DOCTYPE html>
