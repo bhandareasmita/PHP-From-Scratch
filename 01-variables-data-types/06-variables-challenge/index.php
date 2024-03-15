30f7beea01916131414b21bdae9b02e7012b4b6e
Asmita Bhandare <20106482@tafe.wa.edu.au>
2024-03-15 18:59:08 +0800

    Feat: Complete exercise-1.6

--- 01-variables-data-types/06-variables-challenge/index.php
+++ 01-variables-data-types/06-variables-challenge/index.php
@@ -1,19 +1,29 @@
+<?php
+$title = 'Introduction to PHP';
+$author = 'John Doe';
+$body = 'PHP (Hypertext Preprocessor) is a widely used server-side scripting
+        language that has revolutionized web development. With its simplicity,
+        flexibility, and vast community support, PHP has become the backbone of
+        countless dynamic websites and web applications.';
+$pageTitle = 'Brad\'s PHP Blog | ' . $title;
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Brad's PHP Blog | Introduction to PHP</title>
+    <title><?= $pageTitle ?></title>
   </head>
   <body>
     <main>
-      <h1>Introduction to PHP</h1>
-      <p>By: John Doe</p>
+      <h1><?=  $title ?></h1>
+      <p>By: <?= $author ?></p>
       <p>
-        PHP (Hypertext Preprocessor) is a widely used server-side scripting
-        language that has revolutionized web development. With its simplicity,
-        flexibility, and vast community support, PHP has become the backbone of
-        countless dynamic websites and web applications.
+        <?= $body ?>
       </p>
     </main>
   </body>
